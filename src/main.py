951ffd6de76233bbea069de61a2dec015e041478
bruno <b260382@dac.unicamp.br>
2026-05-27 17:07:57 -0300

    add: Adam optimizer

--- src/main.py
+++ src/main.py
@@ -21,14 +21,15 @@ def test_model(model:Model, data_sets):
         plot_loss_curve(model, train_losses, test_losses, dataset_name)
         plot_accuracy_curve(model, train_accs, test_accs, dataset_name)
         plot_decision_boundary(model, X_train, Y_train, X_test, Y_test, dataset_name)
-        plot_internal_decision_boundries(model, X_train, Y_train, X_test, Y_test, dataset_name)
+        #plot_internal_decision_boundries(model, X_train, Y_train, X_test, Y_test, dataset_name)
     
 def define_models():
     models = []
 
+    # Modelo Simples - Inicialização Simple
     modelo_simples = Model(
-        "Modelo_Simples",
-        [LayerDense(2, 6, init="He"), Relu() ,LayerDense(6, 2, init="He")],
+        "Modelo_Simples_Simple",
+        [LayerDense(2, 6, init="Simple"), Relu() ,LayerDense(6, 2, init="Simple")],
         SoftmaxCrossEntropy(),
         SGD(learning_rate=0.1)
     )
@@ -36,21 +37,50 @@ def define_models():
     #O resultados podem não estar entre 0 e 1.
     models.append(modelo_simples)
 
+    # Modelo Simples - Inicialização He
+    modelo_simples_he = Model(
+        "Modelo_Simples_He",
+        [LayerDense(2, 6, init="He"), Relu() ,LayerDense(6, 2, init="He")],
+        SoftmaxCrossEntropy(),
+        SGD(learning_rate=0.1)
+    )
+    models.append(modelo_simples_he)
+
+    # # Modelo Expansivo - Inicialização Simple
+    # modelo_expansivo_simple = Model(
+    #     "Modelo_Expansivo_Simple",
+    #     [FeatureExpansion(), LayerDense(4, 6, init="Simple"), Relu(), LayerDense(6, 2, init="Simple")],
+    #     SoftmaxCrossEntropy(Ridge=2e-6),
+    #     SGD(learning_rate=0.1)
+    # )
+    # models.append(modelo_expansivo_simple)
+
+    # Modelo Expansivo - Inicialização He
     modelo_expansivo = Model(
-        "Modelo_Expansivo",
+        "Modelo_Expansivo_He",
         [FeatureExpansion(), LayerDense(4, 6, init="He"), Relu(), LayerDense(6, 2, init="He")],
         SoftmaxCrossEntropy(Ridge=2e-6),
         SGD(learning_rate=0.1)
     )
     models.append(modelo_expansivo)
 
-    modelo_momentum = Model(
-        "Modelo_Momentum", 
-        [LayerDense(2, 6), Relu(), LayerDense(6, 2)], 
+    # # Modelo Momentum - Inicialização Simple
+    # modelo_momentum = Model(
+    #     "Modelo_Momentum_Simple", 
+    #     [LayerDense(2, 6, init="Simple"), Relu(), LayerDense(6, 2, init="Simple")], 
+    #     SoftmaxCrossEntropy(), 
+    #     SGDMomentum(learning_rate=0.1, beta=0.9)
+    # )
+    # models.append(modelo_momentum)
+
+    # Modelo Momentum - Inicialização He
+    modelo_momentum_he = Model(
+        "Modelo_Momentum_He", 
+        [LayerDense(2, 6, init="He"), Relu(), LayerDense(6, 2, init="He")], 
         SoftmaxCrossEntropy(), 
         SGDMomentum(learning_rate=0.1, beta=0.9)
     )
-    models.append(modelo_momentum)
+    models.append(modelo_momentum_he)
     
     return models
 
